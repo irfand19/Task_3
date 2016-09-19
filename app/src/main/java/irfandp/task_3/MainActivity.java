c816b5524979f40b15006d99895ba2416ab2aa2b
irfand19 <irfand19@gmail.com>
2016-09-19 22:05:26 +0700

    layout home beres

--- app/src/main/java/irfandp/task_3/MainActivity.java
+++ app/src/main/java/irfandp/task_3/MainActivity.java
@@ -1,13 +1,31 @@
 package irfandp.task_3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_login;
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_login :
+                Intent lay_home = new Intent(this, HomeActivity.class);
+                startActivity(lay_home);
+        }
     }
 }
